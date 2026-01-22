2caab847806418984366dcf530c9bd83fb717b6d
pranavgawaii <pranavvgawai@gmail.com>
2026-01-22 19:45:39 +0530

    fix: remove invalid appIsrStatus from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: {
-    appIsrStatus: false,
     buildActivity: false,
     buildActivityPosition: 'bottom-right',
   },
